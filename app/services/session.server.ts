aaa3d9c6f30a16b24224a25cf3aec612453a262b
alejandro <alexandros2707@gmail.com>
2022-05-07 15:51:21 -0400

    fix cypress ts

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -74,3 +74,29 @@ export async function logout(request: Request) {
     },
   });
 }
+
+const USER_SESSION_KEY = "userId";
+
+export async function createUserSession({
+  request,
+  userId,
+  remember,
+  redirectTo,
+}: {
+  request: Request;
+  userId: string;
+  remember: boolean;
+  redirectTo: string;
+}) {
+  const session = await getSession(request);
+  session.set(USER_SESSION_KEY, userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await sessionStorage.commitSession(session, {
+        maxAge: remember
+          ? 60 * 60 * 24 * 7 // 7 days
+          : undefined,
+      }),
+    },
+  });
+}
